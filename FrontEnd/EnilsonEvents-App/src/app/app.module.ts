ebf0bc0d15039f6143b031ea22d7d15bc5457f72
EnilsonNeto <enilsonneto8@gmail.com>
2023-07-10 20:07:28 -0300

    Seção 5: angular, interpolação, diretivas e binding

--- FrontEnd/EnilsonEvents-App/src/app/app.module.ts
+++ FrontEnd/EnilsonEvents-App/src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontEnd/EnilsonEvents-App/src/app/components/eventos/eventos.component.ts
+++ FrontEnd/EnilsonEvents-App/src/app/components/eventos/eventos.component.ts
@@ -8,17 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any
+  eventos: any = [];
+  isCollapsed = false;
+  eventosFiltrados: any = [];
+  private _filter: string = '';
+
+  public get filter(): string{
+    return this._filter;
+  }
+
+  public set filter(value: string) {
+    this._filter = value;
+    this.eventosFiltrados = this.filter ? this.filtrarEventos(this.filter) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.eventos.filter((event: any) =>
+      event.tema.toLowerCase().indexOf(filtrarPor) !== -1 ||
+      event.local.toLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getEvents()
+    this.getEvents();
   }
 
   getEvents() {
     this.http.get('https://localhost:5001/api/Evento').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     );
   }
